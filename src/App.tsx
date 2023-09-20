3b6b74097ee153a4347d0aae75c2f5762cc209ff
Robin Foutel <robin.foutel@gmail.com>
2023-09-20 09:48:56 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Home from "./views/Home";
+import NotFound from "./views/NotFound";
+
+const exclusionArray = [
+  '/login',
+  '/register',
+]
+
+function App() {
+  return (
+    <div className="h-screen flex flex-col">
+      {/*{exclusionArray.indexOf(location.pathname) < 0 && <Header/>}*/}
+      <div className="flex flex-1 overflow-y-auto">
+        <Routes>
+          <Route path="/" >
+            <Route index element={<Home />} />
+            {/*<Route path="/login" element={<Login />} />*/}
+            {/*<Route path="/register" element={<Register />} />*/}
+            <Route path="/*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
